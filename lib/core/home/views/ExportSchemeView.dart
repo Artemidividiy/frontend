f837b1ecb3568b27a8aae00c49351f230e7b89ea
Artemidividiy <artemiy.kasyanik@gmail.com>
2023-01-31 21:31:49 +0300

    qr code generation done

--- lib/core/home/views/ExportSchemeView.dart
+++ lib/core/home/views/ExportSchemeView.dart
@@ -1,5 +1,6 @@
 import 'package:colorful/core/home/viewmodels/HomeViewModel.dart';
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 class ExportSchemeView extends StatefulWidget {
   final HomeViewModel vm;
@@ -36,27 +37,54 @@ class _ExportSchemeViewState extends State<ExportSchemeView> {
                   if (value == null) {
                     return Column(children: [
                       Text("Choose export method"),
-                      Row(
-                        children: [
-                          TextButton(
-                              onPressed: () => vm.copyToClipboard(context),
-                              child: Text("copy to clipboard")),
-                          TextButton(
-                              onPressed: () async {
-                                cssTextNotifier = ValueNotifier<String>(
-                                    await vm.exportScheme());
-                                setState(() {
-                                  height = 400;
-                                });
-                              },
-                              child: Text("export to css"))
-                        ],
-                      )
+                      SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          child: Row(
+                            children: [
+                              TextButton(
+                                  onPressed: () => vm.copyToClipboard(context),
+                                  child: Text("copy to clipboard")),
+                              TextButton(
+                                  onPressed: () async {
+                                    cssTextNotifier = ValueNotifier<String>(
+                                        await vm.exportScheme());
+                                    setState(() {
+                                      height = 400;
+                                    });
+                                  },
+                                  child: Text("export to css")),
+                              TextButton(
+                                  onPressed: () async {
+                                    cssTextNotifier = ValueNotifier<String>(
+                                        "qr-" +
+                                            (await vm.currentScheme)
+                                                .toString());
+                                    setState(() {
+                                      height = 450;
+                                    });
+                                  },
+                                  child: Text("view qr-code"))
+                            ],
+                          ))
                     ]);
+                  } else if (value.contains("qr-")) {
+                    return Padding(
+                      padding: EdgeInsets.all(8),
+                      child: QrImage(
+                        data: value.substring(3),
+                      ),
+                    );
                   } else {
                     return Padding(
                       padding: const EdgeInsets.all(8.0),
-                      child: Text(value),
+                      child: Column(
+                        children: [
+                          Text(value),
+                          IconButton(
+                              onPressed: () => vm.copyToClipboard(context),
+                              icon: Icon(Icons.copy_all))
+                        ],
+                      ),
                     );
                   }
                 })
